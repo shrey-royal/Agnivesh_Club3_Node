a36e0cdc5e5dc1490e5d642f4861a3711ed27f48
Shrey K <srkkadia@gmail.com>
2024-07-28 09:59:49 +0530

    Intro, npm init, basics

---
+++ app.js
@@ -0,0 +1,4 @@
+// const student = require('./basics/students')
+const {studentName, grade} = require('./basics/students')
+
+console.log(studentName, grade);
---
+++ basics/students.js
@@ -0,0 +1,6 @@
+console.log('Student file loaded');
+
+const studentName = "agnivesh"
+const grade = 6
+
+module.exports = {studentName, grade}
